c60ec0cca2409a57350c97ec74603433919e3496
Molok0 <do.proshin@yandex.ru>
2024-02-17 21:50:05 +0500

    add test for CommandsProcessor

---
+++ bot/src/test/java/edu/java/bot/processors/CommandsProcessorTest.java
@@ -0,0 +1,14 @@
+package edu.java.bot.processors;
+
+import edu.java.bot.services.WebSiteProcessorService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+public class CommandsProcessorTest {
+    @InjectMocks
+    private CommandsProcessor commandsProcessor;
+    @Test
+    void process() {
+
+    }
+}
