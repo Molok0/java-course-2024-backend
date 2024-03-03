3a8e18eff8abecb54324400c234d3c7b0f114ee1
DmitryProshin <do.proshin@yandex.ru>
2024-03-04 00:18:41 +0500

    fix checkstyle

--- bot/src/main/java/edu/java/bot/api/client/ScrapperClient.java
+++ bot/src/main/java/edu/java/bot/api/client/ScrapperClient.java
@@ -4,20 +4,25 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 public class ScrapperClient {
+    private static final String LINKS = "/links";
     private final WebClient webClient;
+
     public ScrapperClient(WebClient.Builder webClientBuilder, String defaultUrl) {
         this.webClient = webClientBuilder.baseUrl(defaultUrl).build();
     }
+
     public Mono<Void> regChat(Long id) {
         return this.webClient.post().uri("/tg-chat/{id}", id).retrieve()
             .bodyToMono(Void.class);
     }
+
     public Mono<Void> getLinks() {
-        return this.webClient.get().uri("/links").retrieve()
+        return this.webClient.get().uri(LINKS).retrieve()
             .bodyToMono(Void.class);
     }
+
     public Mono<Void> postLinks() {
-        return this.webClient.post().uri("/links").retrieve()
+        return this.webClient.post().uri(LINKS).retrieve()
             .bodyToMono(Void.class);
     }
 }
