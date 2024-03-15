8d6e3536350516d44734eb3189b81e603529e97f
DmitryProshin <do.proshin@yandex.ru>
2024-03-15 13:38:43 +0500

    change schema

--- migrations/liquibase/sql/schema.sql
+++ migrations/liquibase/sql/schema.sql
@@ -7,10 +7,17 @@ CREATE TABLE IF NOT EXISTS CHAT
 CREATE TABLE IF NOT EXISTS URL
 (
     id             bigint GENERATED ALWAYS AS IDENTITY,
-    uri            TEXT  NOT NULL,
+    url            TEXT  NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS CHAT_URL
+(
+    url_id         BIGINT NOT NULL,
     chat_id        BIGINT NOT NULL,
 
-    PRIMARY KEY (id),
-    FOREIGN KEY (chat_id) REFERENCES CHAT(id)
+    PRIMARY KEY (url_id, chat_id),
+    CONSTRAINT fk_url_id FOREIGN KEY (url_id) REFERENCES URL(id),
+    CONSTRAINT fk_chat_id FOREIGN KEY (chat_id) REFERENCES URL(id)
 );
 
