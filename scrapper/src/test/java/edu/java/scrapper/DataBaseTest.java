34ce3c2303f6491dda2f1a7c3d54b10cdf1df625
DmitryProshin <do.proshin@yandex.ru>
2024-03-16 18:29:33 +0500

    add tests

--- scrapper/src/test/java/edu/java/scrapper/DataBaseTest.java
+++ scrapper/src/test/java/edu/java/scrapper/DataBaseTest.java
@@ -1,4 +1,17 @@
 package edu.java.scrapper;
 
+import org.junit.jupiter.api.Test;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class DataBaseTest extends IntegrationTest{
+    @Test
+    public void testContainerStartup() {
+        try(var conn = POSTGRES.createConnection("")) {
+            assertThat(POSTGRES.isRunning()).isEqualTo(true);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
