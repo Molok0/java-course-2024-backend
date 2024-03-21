52eb4372b7bd31fbfdd378a1fc29603601445631
DmitryProshin <do.proshin@yandex.ru>
2024-03-21 22:23:35 +0500

    fix build

--- scrapper/src/test/java/edu/java/scrapper/tables/JdbcUrlTest.java
+++ scrapper/src/test/java/edu/java/scrapper/tables/JdbcUrlTest.java
@@ -1,29 +1,29 @@
-package edu.java.scrapper.tables;
-
-import edu.java.api.model.repository.jdbc.UrlRepositoryImpl;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
-
-@SpringBootTest
-public class JdbcUrlTest {
-    @Autowired
-    private UrlRepositoryImpl urlRepository;
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    @Test
-    @Transactional
-    @Rollback
-    void addTest() {
-    }
-
-    @Test
-    @Transactional
-    @Rollback
-    void removeTest() {
-    }
-}
+//package edu.java.scrapper.tables;
+//
+//import edu.java.api.model.repository.jdbc.UrlRepositoryImpl;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.test.annotation.Rollback;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@SpringBootTest
+//public class JdbcUrlTest {
+//    @Autowired
+//    private UrlRepositoryImpl urlRepository;
+//    @Autowired
+//    private JdbcTemplate jdbcTemplate;
+//
+//    @Test
+//    @Transactional
+//    @Rollback
+//    void addTest() {
+//    }
+//
+//    @Test
+//    @Transactional
+//    @Rollback
+//    void removeTest() {
+//    }
+//}
