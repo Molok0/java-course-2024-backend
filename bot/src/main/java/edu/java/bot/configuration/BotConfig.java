a63ac03cd25f20f0c5230c99cc9e7aef3b3ba4b8
DmitryProshin <do.proshin@yandex.ru>
2024-04-01 19:56:41 +0500

    fix checkstyle

--- bot/src/main/java/edu/java/bot/configuration/BotConfig.java
+++ bot/src/main/java/edu/java/bot/configuration/BotConfig.java
@@ -2,11 +2,11 @@ package edu.java.bot.configuration;
 
 import com.pengrad.telegrambot.TelegramBot;
 import edu.java.bot.api.client.ScrapperClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import edu.java.bot.processors.url.GitHubProcessor;
 import edu.java.bot.processors.url.StackOverflowProcessor;
 import edu.java.bot.processors.url.UrlProcessor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -30,7 +30,8 @@ public class BotConfig {
     public ScrapperClient scrapperClient(ApplicationConfig applicationConfig) {
         return new ScrapperClient(webClientBuilder, applicationConfig.urlClient().scrapperDefaultUrl());
     }
-        @Bean
+
+    @Bean
     public UrlProcessor urlProcessor() {
         return new GitHubProcessor(new StackOverflowProcessor(null));
     }
