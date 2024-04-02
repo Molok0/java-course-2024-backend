38f3c25611d48fb6d6d9c8f5bf4d26990da57ed0
DmitryProshin <do.proshin@yandex.ru>
2024-04-02 17:20:07 +0500

    update hw4

---
+++ migrations/liquibase/sql/schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS CHAT
+(
+    id  bigint not null,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS URL
+(
+    id             bigint GENERATED ALWAYS AS IDENTITY,
+    url            TEXT  NOT NULL,
+    last_check     TIME NOT NULL,
+
+    PRIMARY KEY (id),
+    CONSTRAINT unique_url UNIQUE (url)
+);
+
+CREATE TABLE IF NOT EXISTS CHAT_URL
+(
+    url_id         BIGINT NOT NULL,
+    chat_id        BIGINT NOT NULL,
+
+    PRIMARY KEY (url_id, chat_id),
+    CONSTRAINT fk_url_id FOREIGN KEY (url_id) REFERENCES URL(id) ON delete cascade ,
+    CONSTRAINT fk_chat_id FOREIGN KEY (chat_id) REFERENCES URL(id) ON delete cascade
+);
+
