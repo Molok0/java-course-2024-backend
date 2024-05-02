26ceee98c14cb682cd9b9a15e09214c42839ad30
DmitryProshin <do.proshin@yandex.ru>
2024-05-02 16:49:55 +0500

    update LinearRetry

--- bot/src/main/java/edu/java/bot/retry/LinearRetry.java
+++ bot/src/main/java/edu/java/bot/retry/LinearRetry.java
@@ -1,13 +1,32 @@
 package edu.java.bot.retry;
 
-import org.reactivestreams.Publisher;
+import java.time.Duration;
+import java.util.function.Predicate;
 import reactor.core.publisher.Flux;
 import reactor.util.retry.Retry;
 
 public class LinearRetry extends Retry {
+    public final Duration minBackoff;
+    public final Duration maxBackoff;
+    public final long maxAttempts;
+
+    public LinearRetry(Duration minBackoff, Duration maxBackoff, long maxAttempts) {
+        this.minBackoff = minBackoff;
+        this.maxBackoff = maxBackoff;
+        this.maxAttempts = maxAttempts;
+    }
+
+    public static LinearRetry filter(Predicate<? super Throwable> errorFilter) {
+        return null;
+    }
+
+    public static LinearRetry maxBackoff(Duration maxBackoff) {
+        return null;
+    }
 
     @Override
-    public Publisher<?> generateCompanion(Flux<RetrySignal> flux) {
+    public Flux<Long> generateCompanion(Flux<RetrySignal> flux) {
+
         return null;
     }
 }
