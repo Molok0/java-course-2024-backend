2cff859988769ebba879960f511064b0d1910b90
DmitryProshin <do.proshin@yandex.ru>
2024-05-05 00:29:10 +0500

    hw8

---
+++ scrapper/src/main/java/edu/java/api/services/ScrapperQueueProducer.java
@@ -0,0 +1,13 @@
+package edu.java.api.services;
+
+import edu.java.api.dto.LinkUpdate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ScrapperQueueProducer {
+    public void send(LinkUpdate update) {
+        // TODO
+    }
+}
